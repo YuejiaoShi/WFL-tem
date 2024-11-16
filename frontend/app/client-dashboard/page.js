24a90a3b1dd25aa091630c85c3004a6f05ae6732
YuejiaoShi <shiyuejiaosyj@gmail.com>
2024-11-17 00:22:27 +0100

    initialize

---
+++ frontend/app/client-dashboard/page.js
@@ -0,0 +1,12 @@
+'use client';
+
+import LogOutButton from '../_components/LogOutButton';
+
+export default function ClientDashboard() {
+  return (
+    <div>
+      <h1>Welcome to the client dashboard!</h1>
+      <LogOutButton />
+    </div>
+  );
+}
